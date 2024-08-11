35dc4e4dd8cfa5affe1c5652053ba257c0769439
MaxGonchar <boborodada@gmail.com>
2024-08-11 17:21:36 +0300

    implement base unit test structure

--- services/ll_manager/src/repos/auth_repo.py
+++ services/ll_manager/src/repos/auth_repo.py
@@ -2,6 +2,7 @@ from src.extensions import db
 from src.models.user import User
 
 
+# TODO: handle user already exists
 def add_user(user: User) -> None:
     db.session.add(user)
     db.session.commit()
